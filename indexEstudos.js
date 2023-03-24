cc21d60490bfaf61856e9dd647150c5f4cefce63
iuna Leon <iuna.leon@valtech.com>
2023-03-24 09:47:18 -0300

    commitando

---
+++ indexEstudos.js
@@ -0,0 +1,9 @@
+import "./src/components/atoms/teste";
+
+const articleListContent = [{
+    title: 'Design',
+    paragraph: 'Agency provides a full service range including technical skills, design.',
+    anchor: 'Learn More',
+
+    
+}]
